13eeed6d4ed9182a829ef317294598e17f1f83c5
Matthew Wycliff <mwycliffh@protonmail.com>
2023-06-19 14:05:37 -0500

    WYCLIFF -- Fine tuning -- Friendly-snippets

--- lua/lazy_snapshot.lua
+++ lua/lazy_snapshot.lua
@@ -38,7 +38,7 @@ return {
   { "nvim-treesitter/nvim-treesitter", commit = "70f161b1dda985d08e106d5e3744b2612755f748", optional = true },
   { "nvim-treesitter/nvim-treesitter-textobjects", commit = "83c59ed1eeae70a55605990993cf4d208948fdf7", optional = true },
   { "onsails/lspkind.nvim", commit = "57610d5ab560c073c465d6faf0c19f200cb67e6e", optional = true },
-  { "rafamadriz/friendly-snippets", commit = "70b727d3454cceb3a818b1746be09786568b7e33", optional = true },
+  { "mwycliffh/friendly-snippets", branch = "wycliff_two" },
   { "rcarriga/cmp-dap", commit = "d16f14a210cd28988b97ca8339d504533b7e09a4", optional = true },
   { "rcarriga/nvim-dap-ui", version = "^3", optional = true },
   { "rcarriga/nvim-notify", version = "^3", optional = true },
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,7 +4,7 @@ return {
     build = vim.fn.has "win32" ~= 0
         and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
       or nil,
-    dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = { "mwycliffh/friendly-snippets" },
     opts = { store_selection_keys = "<C-x>" },
     config = require "plugins.configs.luasnip",
   },
